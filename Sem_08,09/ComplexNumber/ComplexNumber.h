dfb41795283ae6aa1296e5a5b43eaf157eb582a1
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2022-04-30 17:33:20 +0300

    Add files via upload

---
+++ Sem_08,09/ComplexNumber/ComplexNumber.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+
+class ComplexNumber
+{
+private:
+	double real;
+	double im;
+
+public:
+	ComplexNumber();
+	ComplexNumber(double real = 0, double im = 0);
+
+	ComplexNumber getComplexConjugate() const;
+
+	ComplexNumber& operator+=(const ComplexNumber&);
+	ComplexNumber& operator-=(const ComplexNumber&);
+	ComplexNumber& operator*=(const ComplexNumber&);
+	ComplexNumber& operator/=(const ComplexNumber&);
+
+	// These should be friend functions because they are binary functions and their first arguments are NOT objects of our class ComplexNumber.
+	friend std::ostream& operator<<(std::ostream&, const ComplexNumber&);
+	friend std::istream& operator>>(std::istream&, ComplexNumber&);
+};
+
+ComplexNumber operator+(const ComplexNumber&, const ComplexNumber&);
+ComplexNumber operator-(const ComplexNumber&, const ComplexNumber&);
+ComplexNumber operator*(const ComplexNumber&, const ComplexNumber&);
+ComplexNumber operator/(const ComplexNumber&, const ComplexNumber&);
+
+bool operator==(const ComplexNumber&, const ComplexNumber&);
