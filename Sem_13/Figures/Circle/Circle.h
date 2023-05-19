afdd1adf66fa7f5bb1e9c2949e35715ab02eacda
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2023-05-19 14:32:40 +0300

    Add files via upload

---
+++ Sem_13/Figures/Circle/Circle.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Shape.h"
+
+class Circle : public Shape
+{
+private:
+	double radius;
+
+public:
+	Circle(int x, int y, double radius);
+
+	virtual double getArea() const override;
+	virtual double getPerimeter() const override;
+};
