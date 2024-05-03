d5907c4b9ba764dd2a10930a43780853ca6245aa
Ellizzabeth <99212744+Ellizzabeth@users.noreply.github.com>
2024-05-03 13:49:11 +0300

    Update Teacher.h

--- Sem_10/InheritanceExample/Teacher/Teacher.h
+++ Sem_10/InheritanceExample/Teacher/Teacher.h
@@ -2,18 +2,40 @@
 #pragma warning (disable : 4996)
 #include "Person.h"
 
-class Teacher : public Person
+class Teacher: public Person
 {
+
+public:
+	Teacher(const char*name, unsigned int age, const char* const* subjects, unsigned int subjectsCount);
+	Teacher(const Teacher& other);
+	Teacher(Teacher&& other) noexcept;
+	Teacher& operator=(const Teacher& other);
+	Teacher& operator=( Teacher&& other) noexcept;
+	~Teacher();
+
+	//void addAtFirstFreeIndex(const char* el);
+	void removeAt(unsigned int index);
+	void popBack();
+	size_t size() const;
+	bool contains(unsigned int index) const;
+	void setAtIndex(const char* obj, unsigned int index);
+	const char* operator[](unsigned int index) const;
+
+
+
+
 private:
 	char** subjects;
 	unsigned int subjectsCount;
+	unsigned int capacity;
 
 	void free();
 	void copyFrom(const Teacher& other);
+	void moveFrom(Teacher&& other);
 
-public:
-	Teacher(const char* name, unsigned int age, const char* const* subjects, unsigned int subjectsCount);
-	Teacher(const Teacher& other);
-	Teacher& operator=(const Teacher& other);
-	~Teacher();
+	void resize(unsigned int newCapacity);
+	unsigned int getFirstFreeIndex() const;
+
 };
+
+
