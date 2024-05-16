7cca4be3efbf7d956df78a2a67c5783447bd58ca
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2024-05-16 17:06:45 +0300

    Update Factory.cpp

--- Sem_12/ShapeCollection_FactoryMethod/ShapeFactory/Factory.cpp
+++ Sem_12/ShapeCollection_FactoryMethod/ShapeFactory/Factory.cpp
@@ -1,16 +1,35 @@
-#include "Factory.h"
-#include "../Animals/Cat.h"
-#include "../Animals/Dog.h"
-#include "../Animals/Mouse.h"
+#include "ShapeFactory.h"
+#include "../Circle/Circle.h"
+#include "../Triangle/Triangle.h"
+#include "../Rectangle/Rectangle.h"
 
-Animal* AnimalFactory::createAnimal(Animal::AnimalType animalType)
+Shape* ShapeFactory::createShape(int shapeType)
 {
-	switch (animalType)
-	{
-	case Animal::AnimalType::Cat: return new Cat();
-	case Animal::AnimalType::Dog: return new Dog();
-	case Animal::AnimalType::Mouse: return new Mouse();
-	}
-
-	return nullptr;
+	switch (shapeType)
+   	{
+        case Circle::getCircleType():
+        {
+            	std::cout << "Enter data for Circle:" << std::endl;
+            	int x, y;
+            	double radius;
+            	std::cin >> x >> y >> radius;
+            	return new Circle(x, y, radius);
+        }
+        case Triangle::getTriangleType():
+        {
+            	std::cout << "Enter data for Triangle:" << std::endl;
+            	int x1, y1, x2, y2, x3, y3;
+            	std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+            	return new Triangle(x1, y1, x2, y2, x3, y3);
+        }
+        case Rectangle::getRectangleType():
+        {
+            	std::cout << "Enter data for Rectangle:" << std::endl;
+            	int x1, y1, x3, y3;
+	        std::cin >> x1 >> y1 >> x3 >> y3;
+            	return new Rectangle(x1, y1, x3, y3);
+        }
+        default:
+            	return nullptr;
+    	}
 }
