50ee444058c03c856624740e0cf1e59da842aab2
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2024-05-20 21:48:28 +0300

    Update AnimalFactory.hpp

--- Sem_12/AnimalCollection_AbstractFactory/AnimalFactory.hpp
+++ Sem_12/AnimalCollection_AbstractFactory/AnimalFactory.hpp
@@ -1,62 +1,61 @@
 #pragma once
 #include "Animal.h"
 #include <cassert>
-#include <cstring>
 #include <limits>
 #include <string>
 
 class AnimalFactory
 {
 public:
-    static AnimalFactory& getFactory()
-    {
-        static AnimalFactory factory;
-        return factory;
-    }
-
-    void registerAnimalCreator(const AnimalCreator* creator)
-    {
-        assert(count < MAX_CREATORS_COUNT);
-        creators[count++] = creator;
-    }
-
-    Animal* createAnimal(std::istream& is)
-    {
-        char name[32];
-        is >> name;
-
-        const AnimalCreator* creator = getCreator(name);
-        if (creator)
+        static AnimalFactory& getFactory()
         {
-            return creator->createAnimal(is);
+                static AnimalFactory factory;
+                return factory;
         }
-        else 
+
+        void registerAnimalCreator(const AnimalCreator* creator)
+        {    
+                assert(count < MAX_CREATORS_COUNT);
+                creators[count++] = creator;
+        }
+
+        Animal* createAnimal(std::istream& is)
         {
-            is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            return nullptr;
+                char name[32];
+                is >> name;
+
+                const AnimalCreator* creator = getCreator(name);
+                if (creator)
+                {
+                        return creator->createAnimal(is);
+                }
+                else 
+                {
+                        is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        return nullptr;
+                }
         }
-    }
     
 private:
-    static constexpr size_t MAX_CREATORS_COUNT = 10;
-    const AnimalCreator* creators[MAX_CREATORS_COUNT];
-    size_t count;
+        static constexpr size_t MAX_CREATORS_COUNT = 10;
+        const AnimalCreator* creators[MAX_CREATORS_COUNT];
+        size_t count;
     
-    const AnimalCreator* getCreator(const char* str) const
-    {
-        for (size_t i = 0; i < count; i++)
+        const AnimalCreator* getCreator(const char* str) const
         {
-            if (strcmp(creators[i]->getAnimalName(), str) == 0)
-            {
-                return creators[i];
-            }
+                for (size_t i = 0; i < count; i++)
+                {
+                        if (creators[i]->getAnimalName() == str)
+                        {
+                                return creators[i];
+                        }
+                }
+                return nullptr;
         }
-        return nullptr;
-    }
     
-    AnimalFactory() : count(0)
-    {};
-    ~AnimalFactory() = default;
-    AnimalFactory(const AnimalFactory&) = delete;
-    AnimalFactory& operator=(const AnimalFactory&) = delete;
+        AnimalFactory() : count(0)
+        {};
+        ~AnimalFactory() = default;
+        AnimalFactory(const AnimalFactory&) = delete;
+        AnimalFactory& operator=(const AnimalFactory&) = delete;
 };
