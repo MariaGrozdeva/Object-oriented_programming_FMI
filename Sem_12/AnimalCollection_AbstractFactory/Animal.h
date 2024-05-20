df8b9bc60ee6b393a2f565f911e0e78a5daaa4d7
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2024-05-20 21:42:53 +0300

    Update Animal.h

--- Sem_12/AnimalCollection_AbstractFactory/Animal.h
+++ Sem_12/AnimalCollection_AbstractFactory/Animal.h
@@ -6,21 +6,19 @@ class Animal
 {
 public:
 	virtual void sayHello() const = 0;
-
 	virtual Animal* clone()  const = 0;
-
 	virtual ~Animal() = default;
 };
 
 class AnimalCreator
 {
 public:
-    AnimalCreator(const char* animalName);
-    virtual ~AnimalCreator() = default;
-    const char* getAnimalName() const;
+    	AnimalCreator(const char* animalName);
+    	virtual ~AnimalCreator() = default;
+    	const char* getAnimalName() const;
 
-    virtual Animal* createAnimal(std::istream& is) const = 0;
+    	virtual Animal* createAnimal(std::istream& is) const = 0;
 
-protected:
-    std::string animalName;
+private:
+    	std::string animalName;
 };
